d9b044b419c7f75ab2ef28d25bfaa68997455849
Bilal Lolo <blolo@mobiquityinc.com>
2018-03-14 05:41:54 -0400

    added the setup for the 3rd party libraries

--- Lesson1/Exercise1.1/src/index.ts
+++ Lesson1/Exercise1.1/src/index.ts
@@ -1,5 +1,11 @@
+import chalk from  "chalk";
+
+import { spongeMock } from "spongemock";
+
 var weclomeMessage  = function(name:string){
     return 'Hello ' + name + " Welcome to Type script";
 }
 
 console.log(weclomeMessage("Bilal"))
+
+console.log(chalk.blue(spongeMock(weclomeMessage("hbilal"))+ chalk.red('!')));
